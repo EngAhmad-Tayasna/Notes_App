1caec44a3b1e37e83c82bea1550beae921018aa0
Ahmad Tayasnh <engahmadtayansa@gmail.com>
2025-01-03 18:18:26 +0200

    solve problem in model

--- lib/views/widget/notes_view_body.dart
+++ lib/views/widget/notes_view_body.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes/notes_cubit.dart';
 import 'package:notes_app/views/widget/custom_app_bar.dart';
 import 'package:notes_app/views/widget/notes_list_item.dart';
 
-class NotesViewBody extends StatelessWidget {
+class NotesViewBody extends StatefulWidget {
   const NotesViewBody({super.key});
+
+  @override
+  State<NotesViewBody> createState() => _NotesViewBodyState();
+}
+
+class _NotesViewBodyState extends State<NotesViewBody> {
+
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes(); 
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
